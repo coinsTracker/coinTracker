ca6f3b649c1dc9d6104180b90991c39e2c7f67fe
Ranjeet Sodhi <ranjeet@sodhi.org>
2017-09-21 14:46:18 -0400

    updated users

--- server/api/users.js
+++ server/api/users.js
@@ -7,7 +7,8 @@ router.get('/', (req, res, next) => {
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    // attributes: ['name', 'id', 'email'],
+    include:[{all:true, nested:true}]
   })
     .then(users => res.json(users))
     .catch(next)
