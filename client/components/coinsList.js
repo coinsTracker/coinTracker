b287da1e61203d31f745390612d8f16a4541fa76
Ranjeet Sodhi <ranjeet@sodhi.org>
2017-09-22 22:57:40 -0400

    updated sample chart

--- client/components/coinsList.js
+++ client/components/coinsList.js
@@ -10,10 +10,37 @@ import CoinLine from './coinLine'
 class CoinsList extends Component {
   render() {
     const allCoins = this.props.coins
+
+    //Chart data
+    var data = {
+      // A labels array that can contain any sort of values
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+      // Our series array that contains series objects or in this case series data arrays
+      series: [
+        [5, 2, 4, 2, 0]
+      ]
+    };
+
+    new Chartist.Line('.ct-chart', data);
+
+
     return (
       <div className="content">
       <div className="row">
         <div className="col-md-12">
+
+        <div className="card">
+        <div className="header">
+        <h4 className="title">Chart</h4>
+        <p className="category">Investment distribution</p>
+        </div>
+        <div className="content table-responsive table-full-width">
+        {/* RENDER THE CHART */}
+        <div className="ct-chart .ct-minor-sixth"></div>
+        </div>
+        </div>
+
+
           <div className="card">
             <div className="header">
               <h4 className="title">All Coin's</h4>
