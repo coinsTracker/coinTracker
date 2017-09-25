812690f0ad85f7b2ad479806d517e40b8d0bd290
Ranjeet Sodhi <ranjeet@sodhi.org>
2017-09-24 20:26:34 -0400

    Coinhistory Redux store, thunk, express routes and CoinCap.io server API

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/transactions', require('./transactions'))
 router.use('/coins', require('./coins'))
+router.use('/coinHistory', require('./coinHistory'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
